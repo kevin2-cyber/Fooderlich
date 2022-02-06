151f72eaaf621aafdd141f85f25c5e0c76f10b85
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-06 15:36:53 +0000

    fooderlich

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Fooderlich());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
