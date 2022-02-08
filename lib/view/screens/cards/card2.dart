70e53ae31e0035ba4187af49789f2bb137098402
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-08 18:47:32 +0000

    Update card2.dart

--- lib/view/screens/cards/card2.dart
+++ lib/view/screens/cards/card2.dart
@@ -4,17 +4,28 @@ import 'package:flutter/material.dart';
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
 
-  final String category = 'Editor\'s choice';
-  final String title = 'The Art of Dough';
-  final String description = 'Learn to make the perfect bread';
-  final String chef = 'Ray Wenderlich';
-
   @override
   Widget build(BuildContext context) {
     return Center(
       //TODO: Card1 decorate container
       child: Container(
-        color: Colors.yellow,
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag5.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Column(
+          children: const [
+            //TODO: add author information
+            //TODO: add Positioned widget
+          ],
+        ),
       ),
     );
   }
