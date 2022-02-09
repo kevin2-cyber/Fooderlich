13a16a1352bec814777f13afca1e400a07dad546
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-09 00:09:13 +0000

    added a dark grey overlay on the background image

--- lib/view/screens/cards/card3.dart
+++ lib/view/screens/cards/card3.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 
-
 class Card3 extends StatelessWidget {
   const Card3({Key? key}) : super(key: key);
-
-  final String category = 'Editor\'s choice';
-  final String title = 'The Art of Dough';
-  final String description = 'Learn to make the perfect bread';
-  final String chef = 'Ray Wenderlich';
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      //TODO: Card1 decorate container
       child: Container(
-        color: Colors.grey.shade700 ,
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag2.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+              ),
+            ),
+            //TODO: add container, column, iicon and Text
+            //TODO: add center widget with chip widget children
+          ],
+        ),
       ),
     );
   }
