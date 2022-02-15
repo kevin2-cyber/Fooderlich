950b9064e9106ebc6fa6ccb091d4b18db93a9a41
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-15 04:13:01 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'fooderlich_theme.dart';
-import 'home.dart';
 import 'models/models.dart';
+import 'models/profile_manager.dart';
+import 'screens/splash_screen.dart';
+// TODO: Import app_router
 
-void main() => runApp(const Fooderlich());
+void main() {
+  runApp(
+    const Fooderlich(),
+  );
+}
 
-class Fooderlich extends StatelessWidget {
+class Fooderlich extends StatefulWidget {
   const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  _FooderlichState createState() => _FooderlichState();
+}
+
+class _FooderlichState extends State<Fooderlich> {
+  final _groceryManager = GroceryManager();
+  final _profileManager = ProfileManager();
+  // TODO: Create AppStateManager
+  // TODO: Define AppRouter
+
+  // TODO: Initialize app router
+
   @override
   Widget build(BuildContext context) {
-    final theme = FooderlichTheme.dark();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: theme,
-      title: 'Fooderlich',
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => TabManager()),
-          ChangeNotifierProvider(create: (context) => GroceryManager()),
-        ],
-        child: const Home(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => _groceryManager,
+        ),
+        ChangeNotifierProvider(
+          create: (context) => _profileManager,
+        ),
+        // TODO: Add AppStateManager ChangeNotifierProvider
+      ],
+      child: Consumer<ProfileManager>(
+        builder: (context, profileManager, child) {
+          ThemeData theme;
+          if (profileManager.darkMode) {
+            theme = FooderlichTheme.dark();
+          } else {
+            theme = FooderlichTheme.light();
+          }
+
+          return MaterialApp(
+            theme: theme,
+            title: 'Fooderlich',
+            // TODO: Replace with Router widget
+            home: const SplashScreen(),
+          );
+        },
       ),
     );
   }
