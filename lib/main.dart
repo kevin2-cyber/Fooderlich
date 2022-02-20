7fd1023fcb834af5f58033d24e7e058e285602c9
Kelvin Eduful <73193206+kevin2-cyber@users.noreply.github.com>
2022-02-20 13:12:40 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'fooderlich_theme.dart';
 import 'models/models.dart';
-import 'models/profile_manager.dart';
 import 'navigation/app_router.dart';
 
 
