4b100e0c58f5df39d943e625e3876a13a92055b7
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-02 19:04:39 +0000

    added a fromLocation to parse url

--- lib/navigation/app_link.dart
+++ lib/navigation/app_link.dart
@@ -20,6 +20,26 @@ class AppLink {
     this.currentTab,
     this.itemId,
   });
-// TODO: Add fromLocation
+// Add fromLocation
+  static AppLink fromLocation(String? location) {
+    // 1
+    location = Uri.decodeFull(location ?? '');
+    // 2
+    final uri = Uri.parse(location);
+    final params = uri.queryParameters;
+    // 3
+    final currentTab = int.tryParse(params[AppLink.kTabParam] ??
+        '');
+    // 4
+    final itemId = params[AppLink.kIdParam];
+    // 5
+    final link = AppLink(
+      location: uri.path,
+      currentTab: currentTab,
+      itemId: itemId,
+    );
+    // 6
+    return link;
+  }
 // TODO: Add toLocation
 }
