632412c6137adf4f5a4eee3f159a18e7d9d97490
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-02 15:28:52 +0000

    implementation of Shared Preferences

--- lib/models/app_state_manager.dart
+++ lib/models/app_state_manager.dart
@@ -1,47 +1,49 @@
 import 'dart:async';
 
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+import 'app_cache.dart';
 
 class FooderlichTab {
   static const int explore = 0;
   static const int recipes = 1;
   static const int toBuy = 2;
 }
+
 class AppStateManager extends ChangeNotifier {
-  // 2
   bool _initialized = false;
-  // 3
   bool _loggedIn = false;
-  // 4
   bool _onboardingComplete = false;
-  // 5
   int _selectedTab = FooderlichTab.explore;
-  // 6
+  final _appCache = AppCache();
+
   bool get isInitialized => _initialized;
   bool get isLoggedIn => _loggedIn;
   bool get isOnboardingComplete => _onboardingComplete;
   int get getSelectedTab => _selectedTab;
 
-  void initializeApp() {
-    // 7
-    Timer(const Duration(milliseconds: 2000), () {
-      // 8
-      _initialized = true;
-      // 9
-      notifyListeners();
-    },
+  void initializeApp() async {
+    _loggedIn = await _appCache.isUserLoggedIn();
+    _onboardingComplete = await _appCache.didCompleteOnboarding();
+
+    Timer(
+      const Duration(milliseconds: 2000),
+      () {
+        _initialized = true;
+        notifyListeners();
+      },
     );
   }
 
-  void login(String username, String password) {
-    // 10
+  void login(String username, String password) async {
     _loggedIn = true;
-    // 11
+    await _appCache.cacheUser();
     notifyListeners();
   }
 
-  void completeOnboarding() {
+  void completeOnboarding() async {
     _onboardingComplete = true;
+    await _appCache.completeOnboarding();
     notifyListeners();
   }
 
@@ -55,15 +57,12 @@ class AppStateManager extends ChangeNotifier {
     notifyListeners();
   }
 
-  void logout() {
-    // 12
-    _loggedIn = false;
-    _onboardingComplete = false;
+  void logout() async {
     _initialized = false;
     _selectedTab = 0;
-    // 13
+    await _appCache.invalidate();
+
     initializeApp();
-    // 14
     notifyListeners();
   }
 }
