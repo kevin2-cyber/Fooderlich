6a28bc4ba7cfe7bac0aabb18728c1e0af554c8f5
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-03 09:23:18 +0000

    created AppRouteParser that extends RouteInformationParser

---
+++ lib/navigation/app_route_parser.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/navigation/app_link.dart';
+
+class AppRouteParser extends RouteInformationParser<AppLink> {
+  @override
+  Future<AppLink> parseRouteInformation(RouteInformation routeInformation) async {
+
+    final link = AppLink.fromLocation(routeInformation.location);
+    return link;
+  }
+  @override
+  RouteInformation restoreRouteinformation(AppLink appLink) {
+    final location = appLink.toLocation();
+    return RouteInformation(location: location);
+  }
+}
