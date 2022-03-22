01323cbaab7b647ea5d8f73027261ca4ce33fb68
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-22 20:27:38 +0000

    reformated app_route_parser

--- lib/navigation/app_route_parser.dart
+++ lib/navigation/app_route_parser.dart
@@ -3,11 +3,12 @@ import 'package:recipe_app/navigation/app_link.dart';
 
 class AppRouteParser extends RouteInformationParser<AppLink> {
   @override
-  Future<AppLink> parseRouteInformation(RouteInformation routeInformation) async {
-
+  Future<AppLink> parseRouteInformation(
+      RouteInformation routeInformation) async {
     final link = AppLink.fromLocation(routeInformation.location);
     return link;
   }
+
   @override
   RouteInformation restoreRouteInformation(AppLink appLink) {
     final location = appLink.toLocation();
