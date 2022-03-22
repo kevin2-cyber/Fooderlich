f9c7b2953155a5472eb175a5739ffb4f40410fe3
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-22 20:12:49 +0000

    refactoring

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -5,28 +5,20 @@ import '../components/components.dart';
 import '../models/models.dart';
 
 class RecipesScreen extends StatelessWidget {
-
   final exploreService = MockFooderlichService();
 
   RecipesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  FutureBuilder(
-      // 3
+    return FutureBuilder(
       future: exploreService.getRecipes(),
-      builder: (context, AsyncSnapshot<List<SimpleRecipe>>
-      snapshot) {
-        // 4
-        if (snapshot.connectionState == ConnectionState.done)
-        {
+      builder: (context, AsyncSnapshot<List<SimpleRecipe>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
           return RecipesGridView(recipes: snapshot.data ?? []);
-          // 5
           //return const Center(child: Text('Recipes Screen'));
         } else {
-          // 6
-          return const Center(child:
-          CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         }
       },
     );
