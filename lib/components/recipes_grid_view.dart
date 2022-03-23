01319d02d8a4ff6c72bb8e4e302abcce8271efc5
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-23 09:16:15 +0000

    refactored recipe_grid_view

--- lib/components/recipes_grid_view.dart
+++ lib/components/recipes_grid_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/components/components.dart';
-
 import '../models/models.dart';
 
 class RecipesGridView extends StatelessWidget {
-
   final List<SimpleRecipe> recipes;
 
   const RecipesGridView({
@@ -15,20 +13,20 @@ class RecipesGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(
+      padding: const EdgeInsets.only(
         left: 16,
         right: 16,
         top: 16,
-    ),
+      ),
       child: GridView.builder(
         itemCount: recipes.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemBuilder: (context, index) {
-            final simpleRecipe = recipes[index];
-            return RecipeThumbnail(recipe: simpleRecipe);
-          },
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          final simpleRecipe = recipes[index];
+          return RecipeThumbnail(recipe: simpleRecipe);
+        },
       ),
     );
   }
