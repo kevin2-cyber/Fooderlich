1c363d2db1f7c4a7797a06ec188d40e2aa0b76d2
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-23 09:23:50 +0000

    refactored friend_post_list_view

--- lib/components/friend_post_list_view.dart
+++ lib/components/friend_post_list_view.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 import '../models/models.dart';
 import 'components.dart';
 
-class  FriendPostListView extends StatelessWidget {
-
+class FriendPostListView extends StatelessWidget {
   final List<Post> friendPosts;
 
-  const  FriendPostListView({
+  const FriendPostListView({
     Key? key,
     required this.friendPosts,
   }) : super(key: key);
@@ -15,19 +14,18 @@ class  FriendPostListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(
+      padding: const EdgeInsets.only(
         left: 16,
         right: 16,
         top: 0,
-    ),
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Social Chefs ! ',
-              style: Theme.of(context).textTheme.headline1),
+          Text('Social Chefs ! ', style: Theme.of(context).textTheme.headline1),
           // 5
           const SizedBox(height: 16),
-          // TODO: Add PostListView here
+          // Add PostListView here
           ListView.separated(
             // 2
             primary: false,
