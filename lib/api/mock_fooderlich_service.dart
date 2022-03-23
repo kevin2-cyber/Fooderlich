60a2f6994776ed23e15f6bc8189f388bfa0f9f09
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-23 09:50:48 +0000

    refactored mock api service

--- lib/api/mock_fooderlich_service.dart
+++ lib/api/mock_fooderlich_service.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:flutter/services.dart';
-
 import '../models/models.dart';
 
 // Mock recipe service that grabs sample json data to mock recipe request/response
